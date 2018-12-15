cd389b23b15ed039b39bd1eacdf5f71f587e67d9
Roni <ronipaschoal@gmail.com>
2018-12-14 23:58:23 -0200

    Corrigidos: Insersão no histórico; Dentro do tempo estipulado; Somente se estiver a estação e o sensor estiverem ativos.

--- webservice/TempoReal/index.php
+++ webservice/TempoReal/index.php
@@ -1,52 +1,88 @@
 <?php
 
 ob_start();
+require_once(__DIR__ . '/../Util/DBConnection.php');
 require_once(__DIR__ . '/../Util/Database.php');
 
 $postdata = file_get_contents("php://input");
 
-if(isset($postdata) && !empty($postdata)){
+$debug = false;
+
+if((isset($postdata) && !empty($postdata)) OR $debug){
     $request = json_decode($postdata);
-	
-	if(isset($request) && !empty($request)){
+
+	if((isset($request) && !empty($request)) OR $debug){
+
+		if (!$debug) {
+			$codigoNovaLeitura = $request->leitura->codigo;
+			$valorNovaLeitura = str_replace(',', '.', $request->leitura->valor);
+		} else {
+			$codigoNovaLeitura = 'MOV-03';
+			$valorNovaLeitura = 'teste';
+		}
+
+		date_default_timezone_set('America/Sao_Paulo');
+		$dataAtual = date('Y-m-d H:i:s');
+
 		$DBFactory = new Database();
-		$DBFactory->openConnection();	
+		$DBFactory->openConnection();
 		
-		$result = $DBFactory->executeQuery("SELECT id, idEstacao FROM leitura WHERE codigo = '" . $request->leitura->codigo . "'");
+		$result = $DBFactory->executeQuery("SELECT id, idEstacao, ativo FROM leitura WHERE codigo = '" . $codigoNovaLeitura . "'");
 		
 		if(!is_null($result)){
+
 			$idLeitura = $result[0]["id"];
+			$sensorAtivo = $result[0]["ativo"];
 			$idEstacao = $result[0]["idEstacao"];
-			
-			$result = $DBFactory->executeQuery("SELECT COUNT(*) numReg, max(data) as data FROM temporeal WHERE idLeitura = '" . $idLeitura . "'");
 
-			$numReg = $result['numReg']
-			
-			if(!is_null($result)){
+			$result = $DBFactory->executeQuery("SELECT tempoLeitura, ativo FROM estacao WHERE id = '" . $idEstacao . "'");
 
-				date_default_timezone_set('America/Sao_Paulo');
-				$dataAtual = date('Y-m-d H:i:s');
+			$tempoLeitura = $result[0]["tempoLeitura"] / 1000;
+			$estacaoAtiva = $result[0]["ativo"];
 
-				$difData = strtotime($dataAtual) - strtotime($result[0]["data"]);
-	
-				$result = $DBFactory->executeQuery("SELECT tempoLeitura FROM estacao WHERE id = '" . $idEstacao . "'");
+			if ($sensorAtivo && $estacaoAtiva) {
 
-				if($difData > $result['tempoLeitura']){
+				
+				$result = $DBFactory->executeQuery("SELECT COUNT(*) numReg FROM temporeal WHERE idLeitura = '" . $idLeitura . "'");
+				$registers = $result[0]["numReg"];
 
-					if($numReg > 0) {
-						$result = $DBFactory->executeCommand("UPDATE temporeal SET valor = '" . str_replace(',', '.', $request->leitura->valor) . "', data = NOW() WHERE idLeitura = '" . $idLeitura . "'"); 
+				if(!is_null($result)){
+
+					$conn = getConn();
+					
+					if($registers > 0) {
+
+						$result = $DBFactory->executeQuery("SELECT * FROM temporeal WHERE idLeitura = '" . $idLeitura . "'");
+
+						if((strtotime($dataAtual) - strtotime($result[0]["data"])) > $tempoLeitura) {
+
+							$sql = 'INSERT INTO historico (idLeitura, valor, data) VALUES (:idLeitura, :valor, :data)';
+							$stmt = $conn->prepare($sql);
+							$stmt->bindParam("idLeitura", $result[0]["idLeitura"]);
+							$stmt->bindParam("valor", $result[0]["valor"]);
+							$stmt->bindParam("data", $result[0]["data"]);
+							$stmt->execute();
+							$stmt->fetchAll(PDO::FETCH_ASSOC);
+
+							$DBFactory->executeCommand("UPDATE temporeal SET valor = '" . $valorNovaLeitura . "', data = '". $dataAtual ."' WHERE id = '" . $result[0]["id"] . "'");
+						}
+
 					} else {
-						$result = $DBFactory->executeCommand("INSERT INTO temporeal(idLeitura, valor, data) VALUES('" . $idLeitura . "', '" . str_replace(',', '.', $request->leitura->valor) . "', NOW())"); 
-					}
 
-					$result = $DBFactory->executeCommand("INSERT INTO historico(idLeitura, valor, data) VALUES('" . $idLeitura . "', '" . str_replace(',', '.', $request->leitura->valor) . "', NOW())"); 
+						$sql = 'INSERT INTO temporeal (idLeitura, valor, data) VALUES (:idLeitura, :valor, :data)';
+						$stmt = $conn->prepare($sql);
+						$stmt->bindParam("idLeitura",$idLeitura);
+						$stmt->bindParam("valor",$valorNovaLeitura);
+						$stmt->bindParam("data",$dataAtual);
+						$stmt->execute();
+						$stmt->fetchAll(PDO::FETCH_ASSOC);
+					
+					}
 				}
 			}
 		}
 		
 		$DBFactory->closeConnection();
 	}
+
 }
-
-?>
-
