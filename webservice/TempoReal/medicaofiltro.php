1ab2a53d53c693cb6eaf954360d25a136934c407
Roni <ronipaschoal@gmail.com>
2018-12-16 00:07:00 -0200

    correção do sql para pegar da tabela historico

--- webservice/TempoReal/medicaofiltro.php
+++ webservice/TempoReal/medicaofiltro.php
@@ -5,24 +5,24 @@
 	$data = json_decode(file_get_contents('php://input'), true);
 	
 	if(isset($data['data_inicial']) && isset($data['data_final']) && isset($data['id_leitura'])) {
-		$dataInicial = $data['data_inicial'];	
-		$dataFinal = $data['data_final'];	
-		$idLeitura = $data['id_leitura'];		 
+		$dataInicial = $data['data_inicial'];
+		$dataFinal = $data['data_final'];
+		$idLeitura = $data['id_leitura']; 
 	}
 
 	$conn = getConn();
 
-  	$sql = 'SELECT e.nome as estacao_nome, s.nome as sensor_nome, l.nome as leitura_nome, t.valor, l.unidadeMedida, t.data, l.ativo ' .
-											'FROM historico t ' .
+  	$sql = 'SELECT e.nome as estacao_nome, s.nome as sensor_nome, l.nome as leitura_nome, h.valor, l.unidadeMedida, h.data, l.ativo ' .
+											'FROM historico h ' .
 											'JOIN leitura l ' .
-											'ON l.id = t.idLeitura ' .
+											'ON l.id = h.idLeitura ' .
 											'JOIN estacao e ' .
 											'ON e.id = l.idEstacao ' .
 											'JOIN sensor s ' .
-											'ON s.id = l.idLeitura ' .
-											'WHERE t.idLeitura = :idLeitura ' .
-											'AND t.data >= :dataInicial ' .
-											'AND t.data <= :dataFinal';
+											'ON s.id = l.idSensor ' .
+											'WHERE h.idLeitura = :idLeitura ' .
+											'AND h.data >= :dataInicial ' .
+											'AND h.data <= :dataFinal';
   	$stmt = $conn->prepare($sql);
   	$stmt->bindParam("idLeitura",$idLeitura);
   	$stmt->bindParam("dataInicial",$dataInicial);
@@ -30,9 +30,7 @@
   	$stmt->execute();
   	$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  	
   	header('Content-Type: application/json');
-  	echo json_encode($result);		
+  	echo json_encode($result);
 
 ?>
-
